adeb9a6ade816873cf31dd1de2acf21db5b79980
vishal <vishalguptaramasare@gmail.com>
2022-06-27 19:15:58 +0530

    changes by vishal

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid menu1">
+		<div class="row shadow menu">
+			<div class="col-md-2">
+				<div class="logo text-center">
+					<img src="./images/logo.png" style="width: 45%;" alt="Vishal">
+				</div>
+			</div>
+			<div class="col-md-10 ">
+				<div class="row  upperHeader" style="height: 40px;">
+					<div class="align-items-center col-md-12 d-flex justify-content-between pl-5 pr-4">
+						<div class="conntact d-flex">
+							<div class="contactNumber mr-4">
+								<span class="mr-2"><i class="fa-solid fa-phone"></i></span><span>+91
+									9320206598</span>
+							</div>
+							<div class="contactEmail">
+								<span class="mr-2"><i
+										class="fa-solid fa-paper-plane"></i></span><span>vishalvvramasare@gmail.com</span>
+							</div>
+						</div>
+						<div class=" login px-2 py-1 rounded">
+							<span>Login</span><span class="ml-2"><i
+									class="fa-solid fa-right-to-bracket arrowBracket"></i></span>
+						</div>
+					</div>
+				</div>
+				<div class="row ">
+					<div class="offset-3 col-md-9">
+						<ul class="align-items-center d-flex justify-content-around list-unstyled mb-0 menuList"
+							style="height: 40px;">
+							<!-- menu listing  -->
+							<li><a href="#">HOME</a></li>
+							<li><a href="#">ABOUT US</a></li>
+							<li><a href="#">BLOG</a></li>
+							<li><a href="#">SHOP</a></li>
+							<li><a href="#">FRUITS</a></li>
+							<li><a href="#">CONNECT US</a></li>
+							<li>
+								<a href="#"><span><i class="fa-regular fa-heart"></i></span></a>
+								<a href="#" class="px-3"><span><i
+											class="fa-solid fa-bag-shopping"></i></span></a>
+								<a href="#"><span><i class="fa-solid fa-magnifying-glass"></i></span></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
