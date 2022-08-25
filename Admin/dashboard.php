5713a508177a48874dced4642a0218e711e5da7c
vvrvishal <68494216+vvrvishal@users.noreply.github.com>
2022-08-26 02:11:31 +0530

    vishal

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -103,12 +103,12 @@
                             <div class="saleReport">
                                 <h6>Sale Reports</h6>
                                 <div>
-  <canvas id="myChart"></canvas>
-</div>
+                                    <canvas id="myChart" width="400" height="400"></canvas>
+                                </div>
                             </div>
                         </div>
                         <div class="col-5">
-                        <div class="Earning">
+                            <div class="Earning">
                                 <h6>Earning</h6>
                             </div>
                         </div>
@@ -122,29 +122,41 @@
 <script src="../node_modules/chart.js/dist/chart.js"></script>
 <script src="../js/adminJs.js"></script>
 <script>
-    const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  };
-  const myChart = new Chart(document.getElementById('myChart'),config);
-  
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 </script>
+
 </html>
