4ebce2e18974526b17bd263340db48367d4f9195
narendragbtech <narendra@gbtech.in>
2022-09-12 19:02:45 +0530

    vishal

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -42,9 +42,9 @@
     </div> -->
     <div class="mx-0 row">
         <div class="col-12 col-lg-2 col-md-3 pl-2 pr-0">
-            <section class="MenuSection adminMenu bg-white my-1 pt-2 px-2 rounded" style="box-shadow: 0px 0px 6px #9d9d9d;">
+            <section class="MenuSection adminMenu bg-white my-1 pt-2 rounded" style="box-shadow: 0px 0px 6px #9d9d9d;">
                 <!-- user Details  -->
-                <div class="align-items-center d-flex justify-content-between py-1 userDetails">
+                <div class="align-items-center border-bottom d-flex justify-content-between pb-3 pt-3 px-2 userDetails" style="box-shadow: 1px 2px 6px lightgrey;">
                     <div class="d-flex">
                         <img src="../images/logo.png" alt="UserImage" class="border rounded-circle" style="width: 40px; height: 40px;">
                         <div class="UserName ml-2">
@@ -62,18 +62,20 @@
                 <div class="menus">
                     <ul class="list-unstyled mb-0">
                         <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-braille" style="width: 25px;"></i></span>Dashboard</li>
-                        <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-cubes-stacked" style="width: 25px;"></i></span>Products</li>
-                        <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-basket-shopping" style="width: 25px;"></i></span>Sales</li>
+                        <a href="./addProduct.php">
+                            <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-cubes-stacked" style="width: 25px;"></i></span>Products</li>
+                        </a>
+                        <!-- <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-basket-shopping" style="width: 25px;"></i></span>Sales</li> -->
                         <div class="align-items-center d-flex justify-content-between pr-3 menuList">
                             <li class=" pl-3 py-2 "><span class="mr-2"><i class="fa-solid fa-dolly" style="width: 25px;"></i></span>Orders</li>
                             <span class="badge badge-danger">4</span>
                         </div>
-                        <div class="align-items-center d-flex justify-content-between pr-3 menuList">
+                        <!-- <div class="align-items-center d-flex justify-content-between pr-3 menuList">
                             <li class=" pl-3 py-2 "><span class="mr-2"><i class="fa-solid fa-chart-line" style="width: 25px;"></i></span>Analysis</li>
                             <span class="badge badge-danger">4</span>
-                        </div>
-                        <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-money-check-dollar" style="width: 25px;"></i></span>Payout</li>
-                        <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-screwdriver-wrench" style="width: 25px;"></i></span>Setting</li>
+                        </div> -->
+                        <!-- <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-money-check-dollar" style="width: 25px;"></i></span>Payout</li> -->
+                        <!-- <li class=" pl-3 py-2 menuList"><span class="mr-2"><i class="fa-solid fa-screwdriver-wrench" style="width: 25px;"></i></span>Setting</li> -->
                     </ul>
                 </div>
                 <div class="logo_copywrite position-absolute text-center d-none d-md-block" style="bottom: 8px;right: 20%;">
@@ -87,23 +89,23 @@
         </div>
         <div class="col-lg-10 col-md-9 pr-1">
             <div class="bg-white mt-1 rounded" style="box-shadow: 0px 0px 6px #9d9d9d;">
-                <div class="align-items-center border-bottom mb-4 mx-0 pb-3 pt-3 row" style="box-shadow: 0px 1px 6px 0px lightgrey;">
+                <div class="align-items-center border-bottom mb-4 mx-0 pb-3 pt-3 row boxShadow">
                     <div class="col-md-4 ">
                         <div class="pl-4">
                             <h4 class="mb-0">Dashboard</h4>
                             <p class="mb-0 small textLightGray">Everything here</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-5">
                         <div class="align-items-center border d-flex px-2 rounded" style="box-shadow: 0px 0px 3px lightgray;">
                             <span class="textLightGray"><i class="fa-solid fa-magnifying-glass"></i></span>
                             <input type="search" class="border-0 form-control ml-3">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="align-items-center d-flex justify-content-end">
-                            <p class="mb-0 small" style="font-weight: 600;">View Your Store</p>
-                            <span class="ml-2"><i class="fa-solid fa-money-bill-trend-up"></i></span>
+                    <div class="col-md-3">
+                        <div class="align-items-baseline d-flex justify-content-end">
+                            <h6 class="" style="font-weight: 600;">View Your Shop</h6>
+                            <span class="ml-2"><i class="fa-solid fa-shop"></i></span>
                         </div>
                     </div>
                 </div>
@@ -138,43 +140,54 @@
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="d-flex justify-content-between statisticHeader">
+                        <div class="d-flex justify-content-between mb-4 pb-1 statisticHeader">
                             <h6 class="mb-0 ">Statics</h6>
                             <p class="mb-0 mediumFontWeight ml-3 small text-dark"><span class="mr-2"><i class="fa-solid fa-calendar-days"></i></span> Last 30 days</p>
                         </div>
-                        <div class="Order align-items-center border d-flex justify-content-around mt-3 py-3 rounded" style="/* background-color: #aae3e3b5; */box-shadow: 0px 0px 6px #aba8a8;">
-                            <span class="bg-white px-2 py-1 rounded" style=" font-size: larger; "><i class="fa-brands fa-opencart"></i></span>
-                            <div class="">
-                                <p class="h5 mb-0 mb-1 text-capitalize text-muted" style=" font-weight: 600;">Orders</p>
-                                <div class="align-items-baseline d-flex">
-                                    <h5 class="font-weight-bold">254 </h5>
-                                    <span class="ml-2 small text-success" style="font-weight: 500;"><i class="fa-arrow-up fa-solid" style="margin-right: 2px;"></i>12.5%</span>
+                        <div class="align-items-center border mt-3 py-3 rounded row boxShadow">
+                            <div class="col-4 text-center">
+                                <span class="p-2 rounded" style=" font-size: larger;background: #54dede8f; "><i class="fa-brands fa-opencart"></i></span>
+                            </div>
+                            <div class="col-8">
+                                <div class="text-center">
+                                    <p class="h5 text-capitalize text-muted" style=" font-weight: 600;">Orders</p>
+                                    <div class="align-items-baseline d-flex justify-content-around">
+                                        <h5 class="font-weight-bold">254 </h5>
+                                        <span class="ml-2 small text-success" style="font-weight: 500;"><i class="fa-arrow-up fa-solid" style="margin-right: 2px;"></i>12.5%</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="Order align-items-center border py-2 d-flex justify-content-around mt-3 rounded" style="background-color: #c0b4f1e0;">
-                            <span class="bg-white px-2 py-1 rounded" style=" font-size: larger; "><i class="fa-brands fa-opencart"></i></span>
-                            <div class="">
-                                <p class="mb-0 text-muted text-capitalize mb-1" style=" font-weight: 600;">revenge</p>
-                                <div class="align-items-baseline d-flex">
-                                    <h5 class="font-weight-bold"> <i class="fa-solid fa-sack-dollar small"></i> 6350 </h5>
-                                    <span class="ml-2 small text-success" style="font-weight: 500;"><i class="fa-arrow-up fa-solid" style="margin-right: 2px;"></i>7.1%</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="Order align-items-center border py-2 d-flex justify-content-around mt-3 rounded" style="background-color: #f5b2a7f2;">
-                            <span class="bg-white px-2 py-1 rounded" style=" font-size: larger; "><i class="fa-brands fa-opencart"></i></span>
-                            <div class="">
-                                <p class="mb-0 text-muted mb-1 text-capitalize" style=" font-weight: 600;">Earning</p>
-                                <div class="align-items-baseline d-flex">
-                                    <h5 class="font-weight-bold"><i class="fa-regular fa-credit-card small"></i> 2567 </h5>
-                                    <span class="ml-2 small text-danger" style="font-weight: 500;"><i class="fa-arrow-down fa-solid" style="margin-right: 2px;"></i> 0.32%</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="evenue"></div>
-                        <div class="Earning"></div>
 
+                        <div class="align-items-center border mt-3 py-3 rounded row boxShadow">
+                            <div class="col-4 text-center">
+                                <span class="p-2 rounded" style=" font-size: larger;background: #54dede8f; "><i class="fa-brands fa-opencart"></i></span>
+                            </div>
+                            <div class="col-8">
+                                <div class="text-center">
+                                    <p class="h5 text-capitalize text-muted" style=" font-weight: 600;">revenge</p>
+                                    <div class="align-items-baseline d-flex justify-content-around">
+                                        <h5 class="font-weight-bold">6350 </h5>
+                                        <span class="ml-2 small text-success" style="font-weight: 500;"><i class="fa-arrow-up fa-solid" style="margin-right: 2px;"></i>7.1%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="align-items-center border mt-3 py-3 rounded row boxShadow">
+                            <div class="col-4 text-center">
+                                <span class="p-2 rounded" style=" font-size: larger;background: #54dede8f; "><i class="fa-brands fa-opencart"></i></span>
+                            </div>
+                            <div class="col-8">
+                                <div class="text-center">
+                                    <p class="h5 text-capitalize text-muted" style=" font-weight: 600;">Earning</p>
+                                    <div class="align-items-baseline d-flex justify-content-around">
+                                        <h5 class="font-weight-bold">2567 </h5>
+                                        <span class="ml-2 small text-danger" style="font-weight: 500;"><i class="fa-arrow-down fa-solid" style="margin-right: 2px;"></i>0.32%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <!-- Recents Orders  -->
@@ -258,6 +271,68 @@
                             <span class="text-capitalize text-info" style=" font-weight: 500;">View Complete Order <i class="fa-solid small fa-arrow-up-right-from-square"></i> </span>
                     </div>
                 </div>
+
+                <!-- add Products -->
+
+                <div class="row">
+                    <div class="col-6">
+                        <div class="boxShadow">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header" style="background: #0000000f;">
+                                        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="" method="POST">
+                                            <label for="ProductName" class="font-weight-bold w-100">name
+                                                <input type="text" require style="font-weight: 500;" name="ProductName" id="ProductName" class="form-control text-capitalize">
+                                            </label>
+
+                                            <label for="ProductImage" class="font-weight-bold w-100">Image
+                                                <input required type="file" style="font-weight: 500; font-style: italic;" name="ProductImage" id="ProductImage" class="form-control">
+                                            </label>
+
+                                            <label for="ProductCategryLabel" class="font-weight-bold w-100">Categry
+                                                <select name="ProductCategry" required id="ProductCategry" class="form-control">
+                                                    <option value="" >Select Product Categry</option>
+                                                    <option value="Vegetables">Vegetables</option>
+                                                    <option value="Fruits">Fruits</option>
+                                                    <option value="Herbs">Herbs</option>
+                                                </select>
+                                            </label>
+                                            <div class="row">
+                                                <div class="col-6 pr-1 px-0">
+                                                    <label for="Productweight" class="font-weight-bold w-100"> Product Weight
+                                                        <input type="text" name="Productweight" id="Productweight" class="form-control" style="/* width: 45%; */">
+                                                    </label>
+                                                </div>
+                                                <div class="col-6 pl-1 px-0">
+                                                    <label for="Productweight" class="font-weight-bold w-100"> Product Quantity
+                                                        <input type="Number" name="ProductQuantity" id="ProductQuantity" class="form-control" style="/* width: 45%; */">
+                                                    </label>
+                                                </div>
+                                            </div>
+
+                                            <label for="Name" class="font-weight-bold w-100">Price
+                                                <input type="text" required name="ProductPrice" id="FruitsProductPrice" class="w-100 form-control">
+                                            </label>
+
+                                    </div>
+                                    <div class="modal-footer" style="background: #0000000f;">
+                                        <button type="button" class="btn btn-danger text-light btn-sm " data-dismiss="modal">Discard</button>
+                                        <button type="submit" name="submit" class="btn btn-sm AddProductBtn">Add Product</button>
+                                    </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="boxShadow"></div>
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
